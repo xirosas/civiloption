321a4c10d4f915eb0e9069c4411554c3c51ae786
xirosas <33126013+xirosas@users.noreply.github.com>
2018-01-14 01:20:23 -0500

    kk

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,14 @@ function upperCaseF(a){
         a.value = a.value.toUpperCase();
     }, 1);
 }
+
+
+
+var path = "{{ route('autocomplete') }}";
+$('input.typeahead').typeahead({
+    source:  function (query, process) {
+    return $.get(path, { query: query }, function (data) {
+            return process(data);
+        });
+    }
+});
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -174,6 +174,7 @@
 <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('bower_components/bootstrap/dist/js/bootstrap3-typeahead.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('dist/js/adminlte.min.js')}}"></script>
 
